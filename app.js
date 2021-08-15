fbabb4efe2233b6a67e6a887ae52815fbb3af219
Koray Tug <koray.tug@virtudev.net>
2021-08-15 20:54:23 +0300

    add try catch to the main

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
- 
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const uri = process.env.uri || '';
 const dbName = 'TESTAPP';
 
+// async function main() {
+//   const client = new MongoClient(uri);
+//   await client.connect();
+
+//   const admin = client.db(dbName).admin();
+//   console.log(await admin.serverStatus());
+//   console.log(await admin.listDatabases());
+// }
+
+// main();
+
 async function main() {
   const client = new MongoClient(uri);
-  await client.connect();
 
-  const admin = client.db(dbName).admin();
-  console.log(await admin.serverStatus());
-  console.log(await admin.listDatabases());
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    const admin = client.db(dbName).admin();
+    // Make the appropriate DB calls
+    console.log(await admin.serverStatus());
+    console.log(await admin.listDatabases());
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
-main();
+main().catch(console.error);
